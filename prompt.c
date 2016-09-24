2ce894cace184205f953e0f6849fa5adc2531cbf
kinny <1549722424@qq.com>
2016-09-24 19:49:43 +0800

    Use preprocessor statements for c program portability

--- prompt.c
+++ prompt.c
@@ -1,8 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/* If we compiling on Windows compile these functions */
+#ifdef _WIN32
+#include<string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char *readline(char *prompt) {
+    fputs("lispy> ", stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = (char*)malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    //cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+#else
 #include<editline/readline.h>
 #include<editline/history.h>
+#endif
 
 
 int main(int argc, char **argv) {
