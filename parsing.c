bc673605a0b0ad1f6bd6996cf8d5be4fff172a3c
kinny <1549722424@qq.com>
2016-09-30 10:21:01 +0800

    parsing decimal

--- parsing.c
+++ parsing.c
@@ -29,6 +29,8 @@ void add_history(char* unused) {}
 
 int main(int argc, char **argv) {
     /* Create Some Parsers */
+    mpc_parser_t* Decimal   = mpc_new("decimal");
+    mpc_parser_t* Integer   = mpc_new("integer");
     mpc_parser_t* Number    = mpc_new("number");
     mpc_parser_t* Operator  = mpc_new("operator"); 
     mpc_parser_t* Expr      = mpc_new("expr");
@@ -36,15 +38,17 @@ int main(int argc, char **argv) {
 
     /*Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT,
-        "                                                       \
-         number     : /-?[0-9]+/;                               \
-         operator   : '+' | '-' | '*' | '/';                    \
-         expr       : <number> | '(' <operator> <expr>+ ')';    \
-         lispy      : /^/ <operator> <expr>+ /$/;               \
+        "                                                                                               \
+         decimal    : /-?[0-9]+/;                                                                 \
+         integer    : /-?[0-9]+/;                                                                       \
+         number     : <integer> | <decimal>;                                                            \
+         operator   : '+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\";              \
+         expr       : <number> | '(' <operator> <expr>+ ')';                                            \
+         lispy      : /^/ <operator> <expr>+ /$/;                                                       \
         ",
-        Number, Operator, Expr, Lispy);
+        Decimal, Integer, Number, Operator, Expr, Lispy);
 
-    puts("Lispy version 0.0.0.0.1");
+    puts("Lispy version 0.0.0.0.2");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
@@ -70,7 +74,7 @@ int main(int argc, char **argv) {
     }
 
     /* Undefine and Delete our Parser */
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    mpc_cleanup(6, Decimal, Integer, Number, Operator, Expr, Lispy);
 
     return 0;
 }
